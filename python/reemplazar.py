fbb5d1621985a930effb5822c952512bab4c91a8
devcastro9 <castro.q.orlando@gmail.com>
2022-11-03 15:39:37 -0400

    funciona

--- python/reemplazar.py
+++ python/reemplazar.py
@@ -1,23 +1,33 @@
-import re
+import subprocess, sys, re
 
+def obtener_frm():
+    lista_rutas = []
+    subprocess.Popen(['powershell.exe', 'Remove-Item -Recurse -Force forms.txt'], stdout=sys.stdout)
+    powershell_cmd = "Get-ChildItem -Recurse -Filter *.frm | Select FullName > forms.txt"
+    subprocess.Popen(['powershell.exe', powershell_cmd], stdout=sys.stdout)
+    with open('forms.txt', 'r', encoding='utf-16-le') as arch:
+        for row in arch:
+            if row.startswith('D'):
+                lista_rutas.append(row)
+    return lista_rutas
+    
 def adicionar_seg(ruta:str) -> None:
     texto = ''
-    patron_form_load = r"Private Sub Form_Load[^@]*?End Sub"
     try:
         with open(ruta, 'r') as form:
             texto = form.read()
-            print('Original', texto)
             texto = reset_seguridad(texto)
-            print(texto)
             texto = adicion_seguridad(texto)
-            print(texto)
+        with open(ruta, 'w') as form_of:
+            form_of.truncate()
+            form_of.write(texto)
     except Exception as ex:
         print('Ha ocurrido un error:', ex)
     finally:
         print(f'Adicion de seguridad en {ruta}')
 
 def reset_seguridad(texto:str) -> str:
-    patron_seguridad = r"Call SeguridadSet(Me)"
+    patron_seguridad = r"\n\tCall SeguridadSet\(Me\)"
     return re.sub(patron_seguridad, '', texto)
 
 def adicion_seguridad(text:str) -> str:
@@ -27,7 +37,10 @@ def adicion_seguridad(text:str) -> str:
         return text
     cadena_anterior = result[0]
     cadena_nueva = re.sub(r'End Sub', r'\tCall SeguridadSet(Me)\nEnd Sub', cadena_anterior)
-    return re.sub(cadena_anterior, cadena_nueva, text)
+    return re.sub(patron_load, cadena_nueva, text)
 
 if __name__ == '__main__':
-    adicionar_seg('frmLogin.frm')
+    result = obtener_frm()
+    for i in result:
+        print(i)
+    #adicionar_seg(r'D:\\repos\\Sofia\\python\\frmLogin.frm')
